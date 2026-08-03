65d97380907d512cf7036b8f2d4d414313ac6454
MANOEL FARIAS DE OLIVEIRA <105734210+manelZAUM@users.noreply.github.com>
2026-08-03 13:34:20 -0300

    Atualiza implantacao segura do Apps Script

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
+'use strict';
 
-// URL da implantação segura do Google Apps Script.
-// Deve terminar obrigatoriamente em /exec.
 window.APP_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbxih5VRMJgFZKIDjjatvI0Ja0M7E1yl129u9GG57j1LBm8re-V3gPJSXRd6jijJAQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbxfguWeKbW1QMhuhSDrz62dXoQSFAbEj9LAnfOZcXlOWgov8FG-RNI9U9_QCs4wKJo/exec'
 });
